7ce86516a2cc4adc3c27c1a84d7f97988a3bd573
PeterYR <ziyuzhang01@gmail.com>
2025-07-06 10:23:28 -0500

    add demand ratio limits

--- test_scifi/test_scifi.py
+++ test_scifi/test_scifi.py
@@ -28,22 +28,61 @@ fig.add_scatter(
     x=df_line["lmd"],
     y=df_line["exp"],
     mode="lines",
+    line={"color": "darkred"},
     hoverinfo="skip",
-    line={
-        "color": "maroon",
-        "width": 1,
-    },
-    showlegend=False,
+    name="Pareto front",
 )
+
+max_lmd = df_line["lmd"].max()
+max_exp = df_line["exp"].max()
+slope_max = 1 / 0.973642
+slope_min = 1 / 1.565335
+
+fig.add_scatter(
+    # very close to upper right corner of graph
+    x=[0, max_lmd, None, 0, max_lmd],
+    y=[0, max_lmd * slope_max, None, 0, max_lmd * slope_min],
+    mode="lines",
+    line={"color": "gray", "dash": "dot"},
+    hoverinfo="skip",
+    name="Demand ratio limits",
+)
+
+# fig.add_shape(
+#     type="line",
+#     x0=0,
+#     y0=0,
+#     x1=max_lmd,
+#     y1=max_lmd * slope_min,
+#     line={
+#         "color": "gray",
+#         "dash": "dot",
+#     },
+# )
+
+# fig.add_shape(
+#     type="line",
+#     x0=0,
+#     y0=0,
+#     x1=max_lmd,
+#     y1=max_lmd * slope_max,
+#     line={
+#         "color": "gray",
+#         "dash": "dot",
+#     },
+# )
 
 fig.update_layout(
     title="Proof of concept (Scifi calcs, low-end)",
     xaxis_title="LMD/day",
     yaxis_title="EXP/day",
-    legend_title="Base setup type",
+    legend_title=None,
 )
 
-fig.update_traces(textposition="top right")
+fig.update_traces(
+    textposition="top right",
+    marker={"size": 10},
+)
 fig.update_xaxes(
     rangemode="tozero",
     zeroline=True,
